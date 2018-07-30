994deaa65df93ad28e043da4f18159422fc03767
Weller Zhao <weilezhao@gmail.com>
2018-07-30 09:55:17 +0800

    Initial Commit

---
+++ EasySip/EasySip.h
@@ -0,0 +1,19 @@
+//
+//  EasySip.h
+//  EasySip
+//
+//  Created by Weller Zhao on 2018/7/30.
+//  Copyright © 2018 weller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasySip.
+FOUNDATION_EXPORT double EasySipVersionNumber;
+
+//! Project version string for EasySip.
+FOUNDATION_EXPORT const unsigned char EasySipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySip/PublicHeader.h>
+
+
