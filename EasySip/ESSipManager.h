710a5f8d37e9585ef2b1d19f6b5d394a0963e9cb
Weller Zhao <weilezhao@gmail.com>
2018-08-01 15:54:42 +0800

    添加视频播放处理接口 添加来电账号获取

--- EasySip/ESSipManager.h
+++ EasySip/ESSipManager.h
@@ -15,12 +15,13 @@ typedef struct LinphoneCall ESCall;
 extern NSString *const ES_ON_REMOTE_OPEN_CEMERA;
 extern NSString *const ES_ON_CALL_COMMING;
 extern NSString *const ES_ON_CALL_END;
+extern NSString *const ES_ON_CALL_STREAM_UPDATE;
 
 @interface ESSipManager : NSObject
 
 /**
  单例
-
+ 
  @return 返回实例
  */
 + (instancetype) instance;
@@ -28,7 +29,7 @@ extern NSString *const ES_ON_CALL_END;
 
 /**
  登录sip服务器
-
+ 
  @param username 用户名
  @param password 密码
  @param displayName 显示名
@@ -47,7 +48,7 @@ extern NSString *const ES_ON_CALL_END;
 
 /**
  拨打电话
-
+ 
  @param username 用户名
  @param displayName 显示名
  */
@@ -56,7 +57,7 @@ extern NSString *const ES_ON_CALL_END;
 
 /**
  接听电话
-
+ 
  @param call 电话
  */
 - (void) acceptCall: (ESCall*) call;
@@ -69,7 +70,7 @@ extern NSString *const ES_ON_CALL_END;
 
 /**
  配置视频播放流
-
+ 
  @param videoView 视频播放界面
  @param cameraView 当前摄像头显示界面
  */
@@ -82,17 +83,39 @@ extern NSString *const ES_ON_CALL_END;
 - (void) requestOpenCamera;
 
 /**
- 同意打开摄像头
+ 关闭摄像头
+ */
+- (void) closeCamera;
 
+/**
+ 判断当前通话是否开启视频
+ 
+ @param call 通话
+ @return bool
+ */
+- (BOOL) isVideoEnabled: (ESCall*) call;
+
+/**
+ 同意打开摄像头
+ 
  @param call 通话
  */
 -(void) allowToOpenCameraByRemote: (ESCall*)call;
 
 /**
  拒绝打开摄像头
-
+ 
  @param call 通话
  */
 -(void) refuseToOpenCameraByRemote: (ESCall*)call;
+
+
+/**
+ 获取联系人名
+ 
+ @param call 通话
+ @return 名字
+ */
+-(NSString*) getCallName: (ESCall*)call;
 
 @end
--- EasySip/ESSipManager.m
+++ EasySip/ESSipManager.m
@@ -14,6 +14,7 @@
 NSString *const ES_ON_REMOTE_OPEN_CEMERA = @"ES_ON_REMOTE_OPEN_CEMERA";
 NSString *const ES_ON_CALL_COMMING = @"ES_ON_CALL_COMMING";
 NSString *const ES_ON_CALL_END = @"ES_ON_CALL_END";
+NSString *const ES_ON_CALL_STREAM_UPDATE = @"ES_ON_CALL_STREAM_UPDATE";
 
 @implementation ESSipManager
 
@@ -164,13 +165,12 @@ static ESSipManager* _instance = nil;
 - (void) requestOpenCamera {
     
     if (!linphone_core_video_display_enabled(LC))
-        return;
+    return;
     
     LinphoneCall *call = linphone_core_get_current_call(LC);
     if (call) {
         LinphoneCallAppData *callAppData = (__bridge LinphoneCallAppData *)linphone_call_get_user_data(call);
-        callAppData->videoRequested =
-        TRUE; /* will be used later to notify user if video was not activated because of the linphone core*/
+        callAppData->videoRequested = TRUE; /* will be used later to notify user if video was not activated because of the linphone core*/
         LinphoneCallParams *call_params = linphone_core_create_call_params(LC,call);
         linphone_call_params_enable_video(call_params, TRUE);
         linphone_core_update_call(LC, call, call_params);
@@ -180,71 +180,95 @@ static ESSipManager* _instance = nil;
     }
 }
 
+- (void) closeCamera {
+    if (!linphone_core_video_display_enabled(LC))
+    return;
+    [LinphoneManager.instance setSpeakerEnabled:FALSE];
+    
+    LinphoneCall *call = linphone_core_get_current_call(LC);
+    if (call) {
+        LinphoneCallParams *call_params = linphone_core_create_call_params(LC,call);
+        linphone_call_params_enable_video(call_params, FALSE);
+        linphone_core_update_call(LC, call, call_params);
+        linphone_call_params_destroy(call_params);
+    } else {
+        NSLog(@"Cannot toggle video button, because no current call");
+    }
+}
+
+- (BOOL) isVideoEnabled: (ESCall*) call {
+    return linphone_call_params_video_enabled(linphone_call_get_current_params(call));
+}
+
 
 - (void) onCallUpdate: (NSNotification*) notification {
     NSDictionary* userInfo = [notification userInfo];
     NSValue* c = [userInfo valueForKey:@"call"];
-    int state = (int)[userInfo valueForKey:@"state"];
+    //    int state = (int)[userInfo valueForKey:@"state"];
+    LinphoneCallState state = [[userInfo objectForKey:@"state"] intValue];
     NSString* message = [userInfo valueForKey:@"message"];
-    NSLog(@"========== state: %d  %d, message: %@", state, LinphoneCallStateStreamsRunning, message);
+    NSLog(@"========== state: %d, message: %@", state, message);
     LinphoneCall* call = c.pointerValue;
     
     NSDictionary *dict = @{@"call" : [NSValue valueWithPointer:call],
                            @"state" : [NSNumber numberWithInt:state],
                            @"message" : message};
-
+    
     switch (state) {
-        case 18://LinphoneCallIncomingReceived
+            case LinphoneCallIncomingReceived:
             [NSNotificationCenter.defaultCenter postNotificationName:ES_ON_CALL_COMMING object: self userInfo:dict];
-        case LinphoneCallOutgoingInit:
-        case 98://LinphoneCallConnected
-        case 114: {//LinphoneCallStreamsRunning
-            // check video
-            if (!linphone_call_params_video_enabled(linphone_call_get_current_params(call))) {
-                const LinphoneCallParams *param = linphone_call_get_current_params(call);
-                const LinphoneCallAppData *callAppData =
-                (__bridge const LinphoneCallAppData *)(linphone_call_get_user_data(call));
-                if (state == 114 && callAppData->videoRequested &&
-                    linphone_call_params_low_bandwidth_enabled(param)) {
-                    // too bad video was not enabled because low bandwidth
-                    
-                    NSLog(@"带宽太低，无法开启视频通话");
-                    
-                    callAppData->videoRequested = FALSE; /*reset field*/
+            case LinphoneCallOutgoingInit:
+            case LinphoneCallConnected:
+            case LinphoneCallStreamsRunning: {
+                // check video
+                if (![self isVideoEnabled:call]) {
+                    const LinphoneCallParams *param = linphone_call_get_current_params(call);
+                    const LinphoneCallAppData *callAppData =
+                    (__bridge const LinphoneCallAppData *)(linphone_call_get_user_data(call));
+                    if (state == LinphoneCallStreamsRunning && callAppData->videoRequested &&
+                        linphone_call_params_low_bandwidth_enabled(param)) {
+                        // too bad video was not enabled because low bandwidth
+                        
+                        NSLog(@"带宽太低，无法开启视频通话");
+                        
+                        callAppData->videoRequested = FALSE; /*reset field*/
+                    }
                 }
+                [NSNotificationCenter.defaultCenter postNotificationName:ES_ON_CALL_STREAM_UPDATE object:self userInfo:dict];
+                break;
             }
+            case LinphoneCallUpdatedByRemote: {
+                const LinphoneCallParams *current = linphone_call_get_current_params(call);
+                const LinphoneCallParams *remote = linphone_call_get_remote_params(call);
+                
+                /* remote wants to add video */
+                if ((linphone_core_video_display_enabled([LinphoneManager getLc]) && !linphone_call_params_video_enabled(current) &&
+                     linphone_call_params_video_enabled(remote)) &&
+                    (!linphone_core_get_video_policy([LinphoneManager getLc])->automatically_accept ||
+                     (([UIApplication sharedApplication].applicationState != UIApplicationStateActive) &&
+                      floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_9_x_Max))) {
+                         linphone_core_defer_call_update([LinphoneManager getLc], call);
+                         
+                         
+                         [NSNotificationCenter.defaultCenter postNotificationName:ES_ON_REMOTE_OPEN_CEMERA object: self userInfo:dict];
+                         
+                         //                     [self allowToOpenCameraByRemote:call];
+                         
+                     } else if (linphone_call_params_video_enabled(current) && !linphone_call_params_video_enabled(remote)) {
+                         
+                     }
+                break;
+            }
+            case LinphoneCallUpdating:
             break;
-        }
-        case 242: { //LinphoneCallUpdatedByRemote
-            const LinphoneCallParams *current = linphone_call_get_current_params(call);
-            const LinphoneCallParams *remote = linphone_call_get_remote_params(call);
-            
-            /* remote wants to add video */
-            if ((linphone_core_video_display_enabled([LinphoneManager getLc]) && !linphone_call_params_video_enabled(current) &&
-                 linphone_call_params_video_enabled(remote)) &&
-                (!linphone_core_get_video_policy([LinphoneManager getLc])->automatically_accept ||
-                 (([UIApplication sharedApplication].applicationState != UIApplicationStateActive) &&
-                  floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_9_x_Max))) {
-                     linphone_core_defer_call_update([LinphoneManager getLc], call);
-                     
-                     
-                     [NSNotificationCenter.defaultCenter postNotificationName:ES_ON_REMOTE_OPEN_CEMERA object: self userInfo:dict];
-                     
-//                     [self allowToOpenCameraByRemote:call];
-                     
-                 } else if (linphone_call_params_video_enabled(current) && !linphone_call_params_video_enabled(remote)) {
-                     
-                 }
+            case LinphoneCallPausing:
+            case LinphoneCallPaused:
             break;
-        }
-        case LinphoneCallPausing:
-        case LinphoneCallPaused:
+            case LinphoneCallPausedByRemote:
             break;
-        case LinphoneCallPausedByRemote:
-            break;
-        case 210://LinphoneCallEnd
+            case LinphoneCallEnd://LinphoneCallEnd
             [NSNotificationCenter.defaultCenter postNotificationName:ES_ON_CALL_END object: self userInfo:NULL];
-        case LinphoneCallError:
+            case LinphoneCallError:
         default:
             break;
     }
@@ -259,8 +283,16 @@ static ESSipManager* _instance = nil;
 
 -(void) refuseToOpenCameraByRemote: (ESCall*)call {
     LinphoneCallParams *params = linphone_core_create_call_params(LC, call);
+    linphone_call_params_enable_video(params, FALSE);
     linphone_call_accept_update(call, params);
     linphone_call_params_destroy(params);
+}
+
+-(NSString*) getCallName: (ESCall*)call {
+    if (call == NULL)
+    return NULL;
+    LinphoneAddress *addr = linphone_call_get_remote_address(call);
+    return [FastAddressBook displayNameForAddress:addr];
 }
 
 @end
